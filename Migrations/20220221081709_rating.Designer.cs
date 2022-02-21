9c87af3dd0c55ff0e6adce78b9d83ec2fbb5853a
Marino <m.andriamialy@gmail.com>
2022-02-21 11:26:27 +0300

    Ajouter un nouveau champs

--- Migrations/20220221081709_rating.Designer.cs
+++ Migrations/20220221081709_rating.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MyWebApp.Migrations
 {
     [DbContext(typeof(MyWebAppContext))]
-    partial class MyWebAppContextModelSnapshot : ModelSnapshot
+    [Migration("20220221081709_rating")]
+    partial class rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.2");
