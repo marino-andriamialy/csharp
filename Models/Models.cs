139e6f16e2fc17ef0a8c1167b24b4dd625ec13cd
Marino <m.andriamialy@gmail.com>
2022-02-21 10:42:42 +0300

    ajout recherche

--- Models/Models.cs
+++ Models/Models.cs
@@ -1,15 +1,16 @@
 using System.ComponentModel.DataAnnotations;
-
+using System.ComponentModel.DataAnnotations.Schema;
 namespace MyWebApp.Models
 {
     public class Movie
     {
         public int ID { get; set; }
         public string Title { get; set; } = string.Empty;
-
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
